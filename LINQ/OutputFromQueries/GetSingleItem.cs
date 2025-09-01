bca331e0205d2083222f50ebd9c80866da291509
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-01 20:04:51 +0500

    GetLastItemWithPredicate, GetFirstItemWithDefaultAndPredicate and ExpectSingleMatchWithPredicate implemented.

--- LINQ/OutputFromQueries/GetSingleItem.cs
+++ LINQ/OutputFromQueries/GetSingleItem.cs
@@ -4,8 +4,11 @@ namespace LINQ.OutputFromQueries
 	{
 		public override void Run()
 		{
-			GetFirstItem();
+			//GetFirstItem();
 			//GetFirstItemWithPredicate();
+			GetLastItemWithPredicate();
+			GetFirstItemWithDefaultAndPredicate();
+			ExpectSingleMatchWithPredicate();
 		}
 
 		/// <summary>
@@ -32,6 +35,47 @@ namespace LINQ.OutputFromQueries
 
 			var result = sourceMovies
 						 .First(movie => movie.Name.StartsWith("Spider-Man"));
+
+			Print(result);
+		}
+
+		/// <summary>
+		/// You can also retrieve the last item
+		/// This only iterates the entire collections depending on the source.
+		/// Use the version with predicate to maximize efficiency!
+		/// </summary>
+		void GetLastItemWithPredicate()
+		{
+			var sourceMovies = Repository.GetAllMovies();
+
+			var result = sourceMovies
+						 .Last(movie => movie.Name.StartsWith("Spider-Man"));
+
+			Print(result);
+		}
+
+		/// <summary>
+		/// Adding 'OrDefault' prevents exceptions when no item is found.
+		/// </summary>
+		void GetFirstItemWithDefaultAndPredicate()
+		{
+			var sourceMovies = Repository.GetAllMovies();
+
+			var result = sourceMovies
+						 .FirstOrDefault(movie => movie.Name.StartsWith("Batman"));
+
+			Print(result);
+		}
+
+		/// <summary>
+		/// When you only expect a single match, you can throw if more are found.
+		/// </summary>
+		void ExpectSingleMatchWithPredicate()
+		{
+			var sourceMovies = Repository.GetAllMovies();
+
+			var result = sourceMovies
+						 .SingleOrDefault(movie => movie.Name.StartsWith("Spider-Man: Homecoming"));
 
 			Print(result);
 		}
