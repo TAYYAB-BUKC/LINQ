df260677b84b330a9b5ea6fd7c9e5c03b4310b34
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-02 23:39:04 +0500

    Phase property added in Movie model.

--- LINQ.Data/Models/Movie.cs
+++ LINQ.Data/Models/Movie.cs
@@ -5,6 +5,7 @@ namespace LINQ.Data.Models
 		public required Guid MovieId { get; set; }
 		public required string Name { get; set; }
 		public required DateOnly ReleaseDate { get; set; }
+		public int Phase { get; init; }
 		public List<Person> Directors { get; set; } = [];
 		public List<Person> Producers { get; set; } = [];
 
