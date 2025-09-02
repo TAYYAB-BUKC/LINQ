0501a6340275c9ea124ac3ff54366f6809d7b865
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-03 01:25:58 +0500

    CheckIfSequencesMatchWithComparer implemented

--- LINQ/CheckingContents/CompareSequences.cs
+++ LINQ/CheckingContents/CompareSequences.cs
@@ -1,10 +1,13 @@
+using LINQ.Data.Models;
+
 namespace LINQ.CheckingContents
 {
 	public class CompareSequences : QueryRunner
 	{
 		public override void Run()
 		{
-			CheckIfSequencesMatch();
+			//CheckIfSequencesMatch();
+			CheckIfSequencesMatchWithComparer();
 		}
 
 		/// <summary>
@@ -19,6 +22,28 @@ namespace LINQ.CheckingContents
 			var areMoviesEqual = phaseOneMovies.SequenceEqual(firstSixMovies);
 
 			Console.WriteLine(areMoviesEqual);
+		}
+
+		/// <summary>
+		/// Check if the items in 2 sequences, one-by-one, are equal.
+		/// With a custom comparer.
+		/// </summary>
+		void CheckIfSequencesMatchWithComparer()
+		{
+			Movie[] phaseOneMovies = [
+				new Movie { Name = "Iron Man", MovieId = Guid.NewGuid(), ReleaseDate = DateOnly.MinValue },
+				new Movie { Name = "The incredible Hulk", MovieId = Guid.NewGuid(), ReleaseDate = DateOnly.MinValue },
+				new Movie { Name = "Iron Man 2", MovieId = Guid.NewGuid(), ReleaseDate = DateOnly.MinValue },
+				new Movie { Name = "Thor", MovieId = Guid.NewGuid(), ReleaseDate = DateOnly.MinValue },
+				new Movie { Name = "Captain America: The first Avenger", MovieId = Guid.NewGuid(), ReleaseDate = DateOnly.MinValue },
+				new Movie { Name = "The Avengers", MovieId = Guid.NewGuid(), ReleaseDate = DateOnly.MinValue },
+			];
+
+			var firstSixMovies = Repository.GetAllMovies().Take(6);
+
+			var areSequencesEqual = phaseOneMovies.SequenceEqual(firstSixMovies, new MovieComparer());
+
+			Console.WriteLine(areSequencesEqual);
 		}
 	}
 }
