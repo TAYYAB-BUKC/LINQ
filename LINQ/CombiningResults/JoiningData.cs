6e39037a7da3c5735b22dc12f48c308c5ce8627d
Tayyab Arsalan <tayyabdost.bukc@gmail.com>
2025-09-03 10:22:32 +0500

    InnerJoinMultiField implemented using both syntaxes.

--- LINQ/CombiningResults/JoiningData.cs
+++ LINQ/CombiningResults/JoiningData.cs
@@ -5,7 +5,9 @@ namespace LINQ.CombiningResults
 		public override void Run()
 		{
 			//InnerJoin_Q();
-			InnerJoin_F();
+			//InnerJoin_F();
+			//InnerJoinMultiField_Q();
+			InnerJoinMultiField_F();
 		}
 
 		/// <summary>
@@ -40,6 +42,49 @@ namespace LINQ.CombiningResults
 							);
 
 			PrintAll(allCast);
+		}
+
+		/// <summary>
+		/// Inner join data on multiple properties, query syntax
+		/// </summary>
+		void InnerJoinMultiField_Q()
+		{
+			var movieDirectors = Repository.GetAllMovies().SelectMany(
+									movie => movie.Directors,
+									(movie, director) => (Movie: movie, Director: director)
+								);
+
+			var directors = Repository.GetSomeDirectors();
+
+			var result = from movieDirector in movieDirectors
+						 join director in directors
+						 on new { movieDirector.Director.FirstName, movieDirector.Director.LastName }
+						 equals new { director.FirstName, director.LastName }
+						 select (Movie: movieDirector.Movie.Name, Director: director.FullName);
+
+			PrintAll(result);
+		}
+
+		/// <summary>
+		/// Inner join data on multiple properties, fluent syntax
+		/// </summary>
+		void InnerJoinMultiField_F()
+		{
+			var movieDirectors = Repository.GetAllMovies().SelectMany(
+									movie => movie.Directors,
+									(movie, director) => (Movie: movie, Director: director)
+								);
+
+			var directors = Repository.GetSomeDirectors();
+
+			var result = movieDirectors.Join(
+							directors,
+							movieDirector => new { movieDirector.Director.FirstName, movieDirector.Director.LastName },
+							director => new { director.FirstName, director.LastName },
+							(movieDirector, director) => (Movie: movieDirector.Movie.Name, Director: director.FullName)
+						);
+
+			PrintAll(result);
 		}
 	}
 }
